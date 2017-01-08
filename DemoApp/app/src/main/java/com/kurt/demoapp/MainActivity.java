86e97f8217d837f5b6c3740bfe5184560dc87ec4
Unknown <kurty88@hotmail.co.uk>
2017-01-08 15:25:13 +0000

    Image changing. Last thing before start the currency converter challenge.

--- DemoApp/app/src/main/java/com/kurt/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/kurt/demoapp/MainActivity.java
@@ -5,22 +5,45 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void cmdLogIn(View view) {
-        logTextField("Username", findViewById(R.id.text_field_username));
-        logTextField("Password", findViewById(R.id.text_field_password));
+    private ArrayList<Integer> drawableIds;
+    int currentIndex = 0;
+
+    private void incrementIndex(){
+        if(currentIndex < drawableIds.size() - 1){
+            currentIndex++;
+        } else {
+            currentIndex = 0;
+        }
+    }
+
+    public void cmdSwitchImage(View view) {
+        incrementIndex();
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        image.setImageResource(drawableIds.get(currentIndex));
+    }
+
+    private String getTextFromView(View view){
+        EditText textField = (EditText)view;
+        return textField.getText().toString();
     }
 
     private void logTextField(String prefix, View view){
-        EditText textField = (EditText)view;
-        Log.i(prefix, textField.getText().toString());
+        Log.i(prefix, getTextFromView(view));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawableIds = new ArrayList<Integer>();
+        drawableIds.add(R.drawable.cat1);
+        drawableIds.add(R.drawable.cat2);
+        drawableIds.add(R.drawable.cat3);
     }
 }

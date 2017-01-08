7a280d3c771ed01891ac44823f7646be02d7149d
Unknown <kurty88@hotmail.co.uk>
2017-01-08 14:24:51 +0000

    Simple Buttons
    Added some simple buttons to the app for printing text to the log.

--- DemoApp/app/src/main/java/com/kurt/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/kurt/demoapp/MainActivity.java
@@ -2,8 +2,21 @@ package com.kurt.demoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void cmdLogIn(View view) {
+        logTextField("Username", findViewById(R.id.text_field_username));
+        logTextField("Password", findViewById(R.id.text_field_password));
+    }
+
+    private void logTextField(String prefix, View view){
+        EditText textField = (EditText)view;
+        Log.i(prefix, textField.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
